0a1ecef5497ecad1880227ecfe1bcd92245f094a
limengyang-hub <19518898738@163.com>
2026-04-14 14:23:52 +0800

    Initial Commit

---
+++ test01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test01
+//
+//  Created by 1111 on 2026/4/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test01/test01App.swift
@@ -0,0 +1,17 @@
+//
+//  test01App.swift
+//  test01
+//
+//  Created by 1111 on 2026/4/14.
+//
+
+import SwiftUI
+
+@main
+struct test01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
